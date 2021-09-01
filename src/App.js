304e671a971fd1d54d18660c5eb06a309b373290
ayodhya82 <ayodhay82@naver.com>
2021-09-01 15:50:26 +0900

    버튼 클릭 이벤트 예제

--- src/App.js
+++ src/App.js
@@ -1,55 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodLike = [
-  {
-    id:1,
-    name:"kimchi",
-    image:"https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
-    rating:3
-  },{
-    id:2,
-    name:"gimbab",
-    image:"https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg",
-    rating:2
-  },{
-    id:3,
-    name:"sushi",
-    image:"https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F21622F3756405A970C",
-    rating:4
+class App extends React.Component{
+
+  constructor(props){
+    console.log("constructor!!");
+    super(props);
   }
-]
 
-foodLike.PropTypes = {
-  id:PropTypes.number.isRequired,
-  name:PropTypes.string.isRequired,
-  image:PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
+  state = {
+    count : 0
+  }
 
-}
+  componentDidMount(){
+    console.log("componentDidMount!!");
+  }
 
-function Food({name, image, rating}){
-  return (
-    <div>
-      <h1>{name}</h1>
-      <img src={image} alt={name} title={name} />
-      <h4>rating : {rating}</h4>
+  componentDidUpdate(){
+    console.log("componentDidUpdate!!");
+  }
 
-    </div>
+  plus = () =>{
+    this.setState(current => ({count : current.count + 1}));  
+  }
 
-  );
-}
+  minus = () =>{
+    this.setState(current => ({count : current.count -1}));
+    
+  }
 
-function App() {
-  return (
-    <div>
-      <div className="food">
-        {foodLike.map(
-          food => <Food key={food.id} name={food.name} image={food.image} rating={food.rating}/>
-        )}
+  render() {
+    console.log("render!!");
+    const {count} = this.state;
+    return (
+      <div>
+        <h4>this count is {count} </h4>
+        <button onClick={this.plus}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
-    </div>
-  );
+    )
+  }
 }
-
 export default App;
---
+++ src/App_bak1.js
@@ -0,0 +1,57 @@
+/*
+import React from "react";
+import PropTypes from "prop-types";
+
+const foodLike = [
+  {
+    id:1,
+    name:"kimchi",
+    image:"https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
+    rating:3
+  },{
+    id:2,
+    name:"gimbab",
+    image:"https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg",
+    rating:2
+  },{
+    id:3,
+    name:"sushi",
+    image:"https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F21622F3756405A970C",
+    rating:4
+  }
+]
+
+foodLike.PropTypes = {
+  id:PropTypes.number.isRequired,
+  name:PropTypes.string.isRequired,
+  image:PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired
+
+}
+
+function Food({name, image, rating}){
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={image} alt={name} title={name} />
+      <h4>rating : {rating}</h4>
+
+    </div>
+
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <div className="food">
+        {foodLike.map(
+          food => <Food key={food.id} name={food.name} image={food.image} rating={food.rating}/>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+*/
